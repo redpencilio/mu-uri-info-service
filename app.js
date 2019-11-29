5ba30f790113f86d9a8843da44d3b1598a4b4888
Aad Versteden <madnificent@gmail.com>
2019-11-29 17:21:22 +0100

    Initial implementation returning info from a subject
    The current call yields a response for a subject.  The api will be
    extended over time.

--- app.js
+++ app.js
@@ -1,9 +1,23 @@
 // see https://github.com/mu-semtech/mu-javascript-template for more info
 
-import { app, query, errorHandler } from 'mu';
+import { app, query, errorHandler, sparqlEscapeUri } from 'mu';
 
-app.get('/', function( req, res ) {
-  res.send('Hello mu-javascript-template');
+app.get('/', async function( req, res ) {
+  const subject = req.query["subject"];
+
+  const queryResult = await query(`SELECT *
+    WHERE {
+      GRAPH <http://mu.semte.ch/application> {
+        ${sparqlEscapeUri(subject)} ?p ?o.
+      }
+    }
+  `);
+
+  const response = queryResult.results.bindings.map( ({p: { value: predicate }, o }) => {
+    return {subject, predicate, object: o};
+  });
+
+  res.send(response);
 } );
 
 app.use(errorHandler);
