22cbf745b84481b19c425cd2e882cf49cacf4697
Aad Versteden <madnificent@gmail.com>
2019-12-09 13:51:49 +0100

    Yield directed and inverse triple information
    Triple information for a resource can be in two directions.  Sending
    the contents back-and-forth makes it easier to navigate through the
    network.

--- app.js
+++ app.js
@@ -5,7 +5,7 @@ import { app, query, errorHandler, sparqlEscapeUri } from 'mu';
 app.get('/', async function( req, res ) {
   const subject = req.query["subject"];
 
-  const queryResult = await query(`SELECT *
+  const queryResult = await query(`SELECT DISTINCT ?p ?o
     WHERE {
       GRAPH <http://mu.semte.ch/application> {
         ${sparqlEscapeUri(subject)} ?p ?o.
@@ -13,9 +13,22 @@ app.get('/', async function( req, res ) {
     }
   `);
 
-  const response = queryResult.results.bindings.map( ({p: { value: predicate }, o }) => {
-    return {subject, predicate, object: o};
-  });
+  const inverseQueryResult = await query(`SELECT DISTINCT ?s ?p
+    WHERE {
+      GRAPH <http://mu.semte.ch/application> {
+        ?s ?p ${sparqlEscapeUri(subject)}.
+      }
+    }
+  `);
+
+  const response = {
+    directed: queryResult.results.bindings.map( ({p: { value: predicate }, o }) => {
+      return {subject, predicate, object: o};
+    }),
+    inverse: inverseQueryResult.results.bindings.map(({s: {value: inverseSubject}, p: {value: predicate}}) => {
+      return {subject: inverseSubject, predicate, object: subject};
+    })
+  };
 
   res.send(response);
 } );
